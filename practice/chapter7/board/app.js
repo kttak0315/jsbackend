f5a866007f4b5b1311bf37c665d6cad6cca19d4d
kttak0315 <kttak0315@gmail.com>
2025-05-08 13:20:13 +0900

    add.js modified2

--- practice/chapter7/board/app.js
+++ practice/chapter7/board/app.js
@@ -45,7 +45,7 @@ app.get("/modify/:id", async (req, res) => {
   res.render("write", { title: "테스트 게시판 ", mode: "modify", post });
 });
 
-app.post("/modify/", async (req, res) => {
+app.post("/modify", async (req, res) => {
   const { id, title, writer, password, content } = req.body;
 
   const post = {
@@ -55,7 +55,7 @@ app.post("/modify/", async (req, res) => {
     content,
     createdDt: new Date().toISOString(),
   };
-  const result = postService.updatePost(collection, id, post);
+  const result = await postService.updatePost(collection, id, post);
   res.redirect(`/detail/${id}`);
 });
 
@@ -85,7 +85,7 @@ app.post("/check-password", async (req, res) => {
     const { id, password } = req.body;
     const post = await postService.getPostByIdAndPassword(collection, { id, password });
     if (!post) {
-        return res.statusCode(404).json({ isExist: false });
+        return res.status(404).json({ isExist: false });
     } else {
         return res.json({ isExist: true });
     }
