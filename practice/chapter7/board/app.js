7a9929676d79c007bf8e9d2008ad9c1247be4fc1
kttak0315 <kttak0315@gmail.com>
2025-05-08 13:05:55 +0900

    app.js modified

--- practice/chapter7/board/app.js
+++ practice/chapter7/board/app.js
@@ -40,8 +40,24 @@ app.get("/write", (req, res) => {
 });
 
 app.get("/modify/:id", async (req, res) => {
-  const post = await po
-})
+  const post = await postService.getPostById(collection, req.params.id);
+  console.log(post);
+  res.render("write", { title: "테스트 게시판 ", mode: "modify", post });
+});
+
+app.post("/modify/", async (req, res) => {
+  const { id, title, writer, password, content } = req.body;
+
+  const post = {
+    title,
+    writer,
+    password,
+    content,
+    createdDt: new Date().toISOString(),
+  };
+  const result = postService.updatePost(collection, id, post);
+  res.redirect(`/detail/${id}`);
+});
 
 app.post("/write", async (req, res) => {
   const post = req.body;
